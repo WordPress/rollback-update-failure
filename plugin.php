8ec43dcd6de5621f6ee54ac18c77a6e6be944d3c
Andy Fragen <andy@thefragens.com>
2021-02-09 12:06:19 -0800

    bump

--- plugin.php
+++ plugin.php
@@ -11,7 +11,7 @@
  * Plugin Name: Rollback Update Failure
  * Author: Andy Fragen
  * Description: Feature plugin to test plugin/theme update failures and rollback to previous installed packages via zip/unzip.
- * Version: 0.4.4
+ * Version: 0.5.0
  * Network: true
  * License: MIT
  * Text Domain: rollback-update-failure
