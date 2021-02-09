5093d0af483e6b02012ec49cce00fb0180841796
Andy Fragen <andy@thefragens.com>
2021-02-09 09:41:53 -0800

    Revert "include Rollback Testing plugin in this package"
    This reverts commit 497955899dcb50a93e53db301a303db99fb42049.

--- plugin.php
+++ plugin.php
@@ -11,7 +11,7 @@
  * Plugin Name: Rollback Update Failure
  * Author: Andy Fragen
  * Description: Feature plugin to test plugin/theme update failures and rollback to previous installed packages via zip/unzip.
- * Version: 0.4.5
+ * Version: 0.4.4
  * Network: true
  * License: MIT
  * Text Domain: rollback-update-failure
@@ -182,4 +182,3 @@ class Rollback_Update_Failure {
 }
 
 new Rollback_Update_Failure();
-require_once __DIR__ . '/rollback-testing.php';
