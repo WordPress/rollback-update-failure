e4e7cd2668f3593b049b24feb2dbe68389520c40
Andy Fragen <andy@thefragens.com>
2022-03-27 14:51:52 -0700

    unnecessary assignments

--- plugin.php
+++ plugin.php
@@ -649,8 +649,7 @@ class Rollback_Update_Failure {
 
 		// Fetch the runtime environment from a constant, this overrides the global system variable.
 		if ( defined( 'WP_RUNTIME_ENVIRONMENT' ) ) {
-			$current_runtime_env = WP_RUNTIME_ENVIRONMENT;
-			$runtime_constant    = $current_runtime_env;
+			$runtime_constant = WP_RUNTIME_ENVIRONMENT;
 		}
 
 		// Fetch the runtime environment from global system variable.
@@ -658,8 +657,7 @@ class Rollback_Update_Failure {
 			// Check if the runtime environment variable has been set, if `getenv` is available on the system.
 			$has_runtime_env = getenv( 'WP_RUNTIME_ENVIRONMENT' );
 			if ( false !== $has_runtime_env ) {
-				$current_runtime_env = $has_runtime_env;
-				$runtime_getenv      = $current_runtime_env;
+				$runtime_getenv = $has_runtime_env;
 			}
 		}
 
