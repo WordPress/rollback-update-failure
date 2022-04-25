23193c696fa1d3cd514d1e7f337f99563ff06860
Andy Fragen <andy@thefragens.com>
2022-04-25 11:27:00 -0700

    update testing bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -29,7 +29,7 @@ require_once "{$_tests_dir}/includes/functions.php";
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/rollback-update-failure.php';
+	require dirname( dirname( __FILE__ ) ) . '/plugin.php';
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
