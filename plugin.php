1c72aa034b150ab605bf633123d9d57a756e6f6c
Andy Fragen <andy@thefragens.com>
2023-01-18 12:35:22 -0800

    removing some likely never needed stuff

--- plugin.php
+++ plugin.php
@@ -39,31 +39,6 @@ add_action(
 	}
 );
 
-// TODO: Deactivate plugin when/if committed to core.
-// if ( version_compare( get_bloginfo( 'version' ), '6.3-beta1', '>=' ) ) {
-// require_once ABSPATH . 'wp-admin/includes/plugin.php';
-// deactivate_plugins( __FILE__ );
-// }
 
 // Add to wp-admin/includes/admin-filters.php.
 add_action( 'init', array( 'WP_Rollback_Auto_Update', 'init' ) );
-
-// Hopefully add some VirtualBox compatibility.
-add_action(
-	'post_move_dir',
-	function() {
-		/*
-		 * VirtualBox has a bug when PHP's rename() is followed by an unlink().
-		 *
-		 * The bug is caused by delayed clearing of the filesystem cache, and
-		 * the solution is to clear dentries and inodes at the system level.
-		 *
-		 * Most hosts add shell_exec() to the disable_function directive.
-		 * function_exists() is usually sufficient to detect this.
-		 */
-		if ( function_exists( 'shell_exec' ) ) {
-			// phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.system_calls_shell_exec
-			shell_exec( 'sync; echo 2 > /proc/sys/vm/drop_caches' );
-		}
-	}
-);
